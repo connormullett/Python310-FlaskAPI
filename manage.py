e5de2621476a27460c8eeab2223f9c40499c8491
connor <connormullett@gmail.com>
2019-03-07 22:40:52 -0500

    migrate successful

--- manage.py
+++ manage.py
@@ -8,7 +8,7 @@ from src.app import create_app, db
 env_name = os.getenv('FLASK_ENV')
 app = create_app(env_name)
 
-migrate = Migrate(app, db)
+migrate = Migrate(app=app, db=db)
 
 manager = Manager(app=app)
 
--- src/app.py
+++ src/app.py
@@ -3,14 +3,16 @@ from flask import Flask
 
 from .config import app_config
 from .models import db, bcrypt
+from .models import user, blog_post
 
 
-def create_app(env_name):
+def create_app(env_name='development'):
     '''Create app context'''
 
     app = Flask(__name__)
 
     app.config.from_object(app_config[env_name])
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
     bcrypt.init_app(app)
     db.init_app(app)
 
