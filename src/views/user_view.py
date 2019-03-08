989ded12e74499a977525c41a7bd0649fceb7484
connor <connormullett@gmail.com>
2019-03-07 23:20:25 -0500

    added multiple user endpoints

--- src/views/user_view.py
+++ src/views/user_view.py
@@ -9,6 +9,7 @@ from ..share.Authentication import Auth
 
 
 @user_api.route('/', mehtods=['POST'])
+@Auth.auth_required
 def create():
     '''
     Create endpoint for user api
@@ -34,6 +35,44 @@ def create():
     token = Auth.generate_token(ser_data.get('id'))
 
     return custom_response({'token': token}, 201)
+
+
+@user_api.route('/', methods=['GET'])
+@Auth.auth_required
+def get_all():
+    users = UserModel.get_all_users()
+    ser_users = user_schema.dump(users, many=True).data
+    return custom_response(ser_users, 200)
+
+@user_api.route('/login', methods=['POST'])
+def login():
+    '''
+    Validates and returns a web token
+    if the user credentials are verified
+    '''
+    req_data = request.get_json()
+
+    data, error = user_schema.load(req_data, partial=True)
+
+    if error:
+        return custom_response(error, 400)
+
+    if not data.get('email') or not data.get('password'):
+        return custom_response({'error': 'email and password required to login'})
+
+    user = UserModel.get_user_by_email(data.get('email'))
+
+    if not user:
+        return custom_response({'error': 'invalid credentials'})
+
+    if not user.check_hash(data.get('password')):
+        return custom_response({'error': 'invalid credentials'})
+
+    ser_data = user_schema.dump(user).data
+
+    token = Auth.generate_token(ser_data.get('id'))
+
+    return custom_response({'token': token}, 200)
 
 
 def custom_response(res, status_code):
