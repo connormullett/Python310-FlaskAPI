b81d4c011818c9f3eebed9d099955c445a612a17
connormullett <connormullett@gmail.com>
2019-04-08 16:41:18 -0400

    moved app out of script guard

--- app.py
+++ app.py
@@ -5,9 +5,9 @@ import os
 from src.app import create_app
 from flask_heroku import Heroku
 
+env_name = os.getenv('FLASK_ENV')
+app = create_app(env_name)
+heroku = Heroku(app)
 
 if __name__ == '__main__':
-    env_name = os.getenv('FLASK_ENV')
-    app = create_app(env_name)
-    heroku = Heroku(app)
     app.run()
