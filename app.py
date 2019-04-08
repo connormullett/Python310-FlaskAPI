4c466293bc6bf459a55bd0b9fff3c4f49f60b5bc
connormullett <connormullett@gmail.com>
2019-04-08 15:46:03 -0400

    file changes

--- app.py
+++ app.py
@@ -3,9 +3,11 @@
 import os
 
 from src.app import create_app
+from flask_heroku import Heroku
 
 
 if __name__ == '__main__':
     env_name = os.getenv('FLASK_ENV')
     app = create_app(env_name)
+    heroku = Heroku(app)
     app.run()
