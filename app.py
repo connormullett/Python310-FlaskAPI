89c0d370ad11d9023f68b548cd674d160c533eca
connormullett <connormullett@gmail.com>
2019-04-15 13:26:58 -0400

    added procfile

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ from flask_heroku import Heroku
 
 env_name = os.getenv('FLASK_ENV')
 app = create_app(env_name)
-# heroku = Heroku(app)
+heroku = Heroku(app)
 
 if __name__ == '__main__':
     app.run()
